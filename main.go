fc8d265ab8ff696bfe9aa934885f9f0549c8a776
jsonLiang <yuanyu90221@gmail.com>
2020-04-22 23:13:57 +0800

    add .

--- main.go
+++ main.go
@@ -6,7 +6,19 @@ import (
 	_ "github.com/yuanyu90221/gotest/foo"
 )
 
+const (
+	Monday = iota
+	Tuesday
+	Wednesday
+)
+
 func main() {
+	foo := "Hello"
+	bar := 10
+	fmt.Println(foo)
+	fmt.Println(bar)
+	fmt.Println(Monday)
+	fmt.Println(Tuesday)
 	// fmt.Println(bar.HelloWorld())
 	fmt.Println("Hello Main")
 }
