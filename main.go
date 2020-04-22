7a051176b4d059d0afa2987ed651f959897e5063
jsonLiang <yuanyu90221@gmail.com>
2020-04-22 23:24:42 +0800

    function

--- main.go
+++ main.go
@@ -6,19 +6,37 @@ import (
 	_ "github.com/yuanyu90221/gotest/foo"
 )
 
-const (
-	Monday = iota
-	Tuesday
-	Wednesday
-)
+func add(i, j int) int {
+	return i + j
+}
 
+func swap(i, j int) (int, int) {
+	return j, i
+}
+
+func foo() func() int {
+	return func() int {
+		return 100
+	}
+}
 func main() {
-	foo := "Hello"
-	bar := 10
-	fmt.Println(foo)
-	fmt.Println(bar)
-	fmt.Println(Monday)
-	fmt.Println(Tuesday)
-	// fmt.Println(bar.HelloWorld())
-	fmt.Println("Hello Main")
+	fmt.Println(add(1, 2))
+	a := 1
+	b := 2
+	a, b = swap(a, b)
+	fmt.Println("a:", a, "b:", b)
+
+	a, b = b, a
+	fmt.Println("a:", a, "b:", b)
+
+	bar := foo()
+	fmt.Printf("%T\n", bar)
+	fmt.Println(bar())
+
+	bar2 := func(i, j float32) float32 {
+		return i + j
+	}
+
+	fmt.Printf("%T\n", bar2)
+	fmt.Println(bar2(1, 45+2.3))
 }
