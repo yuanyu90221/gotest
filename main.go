5e944131705ea0d349510229d219451c7e9e77e7
jsonLiang <yuanyu90221@gmail.com>
2020-04-23 23:42:30 +0800

    add slice

--- main.go
+++ main.go
@@ -4,37 +4,30 @@ import (
 	"fmt"
 )
 
-type errUserNameExist struct {
-	UserName string
+func addValue(foo []string) []string {
+	foo = append(foo, "c")
+	fmt.Println("add value", foo)
+	return foo
+}
+func modify(foo []string) {
+	foo[1] = "c"
+	fmt.Println("modify foo", foo)
 }
 
-func (e errUserNameExist) Error() string {
-	return fmt.Sprintf("username %s already exist", e.UserName)
-}
-func isErrUserNameExist(err error) bool {
-	_, ok := err.(errUserNameExist)
-	return ok
-}
-func checkUserNameExist(username string) (bool, error) {
-	if username == "bar" {
-		return true, errUserNameExist{UserName: username}
-	}
-	if username == "foo" {
-		return true, errUserNameExist{UserName: username}
-	}
-	return false, nil
-}
 func main() {
-	if _, err := checkUserNameExist("foo"); err != nil {
-		if isErrUserNameExist(err) {
-			fmt.Println(err)
-		}
-	}
-	if _, err := checkUserNameExist("bar"); err != nil {
-		if isErrUserNameExist(err) {
-			fmt.Println(err)
-		} else {
-			fmt.Println("isErrUserNameExist is false")
-		}
-	}
+	foo := []string{"a", "b"}
+	fmt.Println("before foo:", foo)
+	addValue(foo)
+	fmt.Println("after foo:", foo)
+	bar := foo[:1]
+	fmt.Println(" bar:", bar)
+	s1 := append(bar, "c")
+	fmt.Println("foo:", foo)
+	fmt.Println("s1", s1)
+	s2 := append(bar, "d")
+	fmt.Println("foo:", foo)
+	fmt.Println("s2:", s2)
+	s3 := append(bar, "e", "f")
+	fmt.Println("foo:", foo)
+	fmt.Println("s3:", s3)
 }
